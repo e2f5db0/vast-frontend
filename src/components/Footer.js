3f152f1598dcb1a51ee547913a4ea7b9e5ca86a6
universami <sami.toiskallio@protonmail.com>
2019-10-20 16:17:03 +0300

    add a footer to the main page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p>&#9400; 2020 Universami</p>
+        </div>
+    )
+}
+
+export default Footer
