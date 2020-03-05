aa8656b113d4835264746118bee1800f80024299
universami <sami.toiskallio@protonmail.com>
2020-03-05 12:06:02 +0200

    automatically detect when line.wav ends & render choice buttons. minor refactoring in Canvas.js

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import Header from './Header'
 import Line from './Line'
 import Button from './Button'
+import ReactAudioPlayer from 'react-audio-player'
 import lineService from '../services/lineService'
 
 const Canvas = ({ baseurl, initial_delay, i, setCanvas, sCount, setSCount, setEnd, achievements }) => {
@@ -18,13 +19,6 @@ const Canvas = ({ baseurl, initial_delay, i, setCanvas, sCount, setSCount, setEn
     const [cCount, setCCount] = useState('')
     const [rCount, setRCount] = useState('')
 
-    const render_achievement = (ending, delay) => {
-        setTimeout(() => {
-            setEnd(ending)
-            setCanvas(false)
-        }, delay * 1000)
-    }
-
     useEffect(() => {
         lineService.get_count_left()
             .then((count) => setLCount(count))
@@ -36,53 +30,60 @@ const Canvas = ({ baseurl, initial_delay, i, setCanvas, sCount, setSCount, setEn
             .then((count) => setRCount(count))
     }, [])
 
+    // handle the first Line
     if (path === '') {
         setTimeout(() => {
             setShowChoices(true)
         }, initial_delay * 1000)
     }
-    if (path === 'left' && index === lCount+1) {
+
+    // handle the endings
+    const render_achievement = (ending, delay) => {
+        setTimeout(() => {
+            setEnd(ending)
+            setCanvas(false)
+        }, delay * 1000)
+    }
+    if (path === 'left' && index === lCount + 1) {
         render_achievement('rotten_religion', 22)
     }
-    if (path === 'center' && index === cCount+1) {
+    if (path === 'center' && index === cCount + 1) {
         render_achievement('meet_your_death', 4)
     }
-    if (path === 'right' && index === rCount+1) {
+    if (path === 'right' && index === rCount + 1) {
         render_achievement('tale_of_creation', 18)
     }
 
-    // show the choices after the audio has stopped playing
-    const handleChoices = (time) => {
-        setTimeout(() => {
-            setShowChoices(true)
-        }, time * 1000)
-    }
-
-    // fetches the next line from the database when a choice is clicked
+    // fetches the next line from the database when a choice is made
     const next_line = async (path) => {
         const l = await axios.get(baseurl + path + '-path/' + String(index))
         setShowChoices(false)
-        handleChoices(l.data.duration)
         const choices = [
             l.data.choice1,
             l.data.choice2,
             l.data.choice3
         ]
         const text = l.data.text
-        update_states(choices, text)
-        new Audio(baseurl + 'lines/' + path + '_path' + String(index) + '.wav').play()
-    }
-
-    const update_states = (choices, text) => {
         setLine(text)
         setChoices(choices)
         setIndex(index + 1)
+    }
+
+    const handleFirstChoice = (path) => {
+        setPath(path)
+        next_line(path)
     }
 
     return (
         <div className='App'>
             <div className='Canvas'>
                 <Header className='Header' moving={false} />
+                {
+                    path !== '' &&
+                    <ReactAudioPlayer src={baseurl + 'lines/' + path + '_path' + String(index-1) + '.wav'} autoPlay onEnded={() => {
+                        setShowChoices(true)
+                    }} />
+                }
                 <div>
                     <div>
                         <Line text={line} />
@@ -90,20 +91,17 @@ const Canvas = ({ baseurl, initial_delay, i, setCanvas, sCount, setSCount, setEn
                     {showChoices === true && <div>
                         <Button type='Choice-button' text={choices[0]} handleClick={() => {
                             if (path === '') {
-                                setPath('left')
-                                next_line('left')
+                                handleFirstChoice('left')
                                 return
                             }
                             next_line(path)
                         }} />
                         <Button type='Choice-button' text={choices[1]} handleClick={() => {
                             if (path === '' && achievements.length > 0) {
-                                setPath('center')
-                                next_line('center')
+                                handleFirstChoice('center')
                                 return
-                            } else if (path === '' && achievements.length === 0) {
-                                setPath('right')
-                                next_line('right')
+                            } else if (path === '' && achievements.length === 0) { // choosing the center path first is disallowed
+                                handleFirstChoice('right')
                             }
                             next_line(path)
                         }} />
@@ -112,8 +110,7 @@ const Canvas = ({ baseurl, initial_delay, i, setCanvas, sCount, setSCount, setEn
                                 setSCount(sCount + 1)
                             }
                             if (path === '') {
-                                setPath('right')
-                                next_line('right')
+                                handleFirstChoice('right')
                                 return
                             }
                             next_line(path)
