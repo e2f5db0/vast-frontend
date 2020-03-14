7f3894341aecff665baf29fa0722f2e31df35145
universami <sami.toiskallio@protonmail.com>
2020-03-14 12:50:45 +0200

    modify the final message in Canvas.js

--- src/components/FinalMessage.js
+++ src/components/FinalMessage.js
@@ -9,9 +9,10 @@ const FinalMessage = () => {
                     [
                         'Hello, friend.', 1000,
                         'Hello, human.', 3000,
-                        'There is nothing for you here anymore.', 6000,
+                        'You saved the world, but...', 5000,
+                        'There is nothing for you here.', 6000,
                         'Scamper off now.', 4000,
-                        'Go on.', 4000,
+                        'Go on.', 5000,
                         'Everything that has a beginning', 5000,
                         'Everything that has a beginning has an end', 6000,
                         'Everything that has a beginning has an end?'
